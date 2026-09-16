b2e91881cbba0e423dd8708f7090ed84da7fe2ee
davstore4-del <mamykalume58@gmail.com>
2026-09-16 07:20:21 +0200

    Corriger les imports Firebase (URLs CDN au lieu de noms de paquets npm)

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,5 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAY-UZX7zRz-mP4F0VMNTPwW6nFFeaZdUg",
